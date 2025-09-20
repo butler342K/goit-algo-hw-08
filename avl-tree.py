a3eee7aa662f0aaebe88b4fc18d06cebec5ac4bd
Vitalii Kichenko <vkichenko@gmail.com>
2025-09-20 13:08:21 +0300

    avl-tree done

--- avl-tree.py
+++ avl-tree.py
@@ -5,6 +5,14 @@ class AVLNode:
         self.left = None
         self.right = None
 
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\t" * level + prefix + str(self.key) + "\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
 def get_height(node):
     if not node:
         return 0
@@ -39,6 +47,12 @@ def right_rotate(y):
 
     return x
 
+def min_value_node(node):
+    current = node
+    while current.left is not None:
+        current = current.left
+    return current
+
 def insert(root, key):
     if not root:
         return AVLNode(key)
@@ -69,3 +83,71 @@ def insert(root, key):
             return left_rotate(root)
 
     return root
+
+def delete_node(root, key):
+    if not root:
+        return root
+
+    if key < root.key:
+        root.left = delete_node(root.left, key)
+    elif key > root.key:
+        root.right = delete_node(root.right, key)
+    else:
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+
+        temp = min_value_node(root.right)
+        root.key = temp.key
+        root.right = delete_node(root.right, temp.key)
+
+    if root is None:
+        return root
+
+    root.height = 1 + max(get_height(root.left), get_height(root.right))
+
+    balance = get_balance(root)
+
+    if balance > 1:
+        if get_balance(root.left) >= 0:
+            return right_rotate(root)
+        else:
+            root.left = left_rotate(root.left)
+            return right_rotate(root)
+
+    if balance < -1:
+        if get_balance(root.right) <= 0:
+            return left_rotate(root)
+        else:
+            root.right = right_rotate(root.right)
+            return left_rotate(root)
+
+    return root
+
+# Perform inorder traversal and collect nodes
+def inorder_traversal(node):
+    if node is None:
+        return []
+    return inorder_traversal(node.left) + [node] + inorder_traversal(node.right)
+
+# Driver program to test the above functions
+root = None
+keys = [10, 20, 30, 25, 28, 27, -1, 5, 6,-3]
+
+for key in keys:
+    root = insert(root, key)
+    print("AVL-Дерево:")
+    print(root)
+
+
+min_value = min_value_node(root)
+print("Мінімальне значення в дереві:", min_value.key)
+sum_values = sum(node.key for node in inorder_traversal(root))
+print("Сума всіх значень в дереві:", sum_values)
+
+
